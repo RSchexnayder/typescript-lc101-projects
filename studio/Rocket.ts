9c3ec0a1108423ec0bc773df7b6309fd72c47799
Rayna Schexnayder <Rayna.schexnayder@gmail.com>
2019-10-21 22:19:20 -0500

    Debugged initial compilation. need to debug running js

---
+++ studio/Rocket.ts
@@ -0,0 +1,52 @@
+import { Payload } from './Payload.js';
+import { Astronaut } from './astronaut';
+import { Cargo } from './cargo';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+
+    constructor(name: string, totalCapacityKg: number){
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number{
+        let total: number = 0;
+        for (let i: number = 0; i < items.length; i++){
+            total += items[i].massKg;
+
+        }
+        return total;
+    }
+    currentMassKg(): number {
+        let astronautMass: number = this.sumMass(this.astronauts);
+        let cargoMass: number = this.sumMass(this.cargoItems);
+        return astronautMass + cargoMass;
+    }
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true){
+            this.cargoItems.push(cargo);
+            return true
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true){
+            this.astronauts.push(astronaut);
+            return true;
+        } else{
+            return false;
+        }
+    }
+ }
